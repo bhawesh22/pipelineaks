ba410b3d3cfe6a7fe2ef3f753b9f9d88306c2f85
unknown <bhawesh.mishra@kapilshrivastavaG20outlook.onmicrosoft.com>
2025-08-13 14:13:48 +0530

    new aks code

---
+++ modules/azurerm_aks_cluster/variable.tf
@@ -0,0 +1,5 @@
+variable "resource_group_name" {}
+variable "resource_group_location" {} 
+variable "azurerm_kubernetes_cluster" {}
+variable "dns_prefix" {}
+variable "kubernetes_version" {}
---
+++ modules/azurerm_resource_group/main.tf
@@ -0,0 +1,8 @@
+
+
+resource "azurerm_resource_group" "resource" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+  
+
+}
