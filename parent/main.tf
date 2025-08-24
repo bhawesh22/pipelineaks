01c7fb2ae3334123ae8b41959335929ef178c25a
bhawesh22 <mail2bhaweshmishra@gmail.com>
2025-08-24 23:27:47 +0530

    Update main.tf

--- parent/main.tf
+++ parent/main.tf
@@ -1,17 +1,6 @@
 module "resource_group" {
   source                  = "../modules/azurerm_resource_group"
-  resource_group_name     = "bhawesh_n"
-  resource_group_location = "WestUS"
-}
-module "resource_group" {
-  source                  = "../modules/azurerm_resource_group"
-  resource_group_name     = "bhawesh_Y"
-  resource_group_location = "WestUS"
-}
-
-module "resource_group" {
-  source                  = "../modules/azurerm_resource_group"
-  resource_group_name     = "bhawesh_Y"
+  resource_group_name     = "bhawesh_s"
   resource_group_location = "WestUS"
 }
 
@@ -19,7 +8,7 @@ module "aks_cluster" {
   source                  = "../modules/azurerm_aks_cluster"
   depends_on              = [module.resource_group]
 
-  resource_group_name     = "bhawesh_n"
+  resource_group_name     = "bhawesh_s"
   resource_group_location = "WestUS"
   azurerm_kubernetes_cluster  = "bhawesh_aks_cluster"
   dns_prefix              = "bhaweshdns"
